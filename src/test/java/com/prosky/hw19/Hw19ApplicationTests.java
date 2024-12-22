90cca8b205b704fa2f0f309fd290371d168ab68d
nkrenta <nkrenta@inbox.ru>
2024-12-22 12:12:12 +0300

    Init

---
+++ src/test/java/com/prosky/hw19/Hw19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosky.hw19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
